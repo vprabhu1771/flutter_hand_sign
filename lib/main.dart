ba3169767a61d27e7bb22c9daae94232884b81e9
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-12 12:52:08 +0530

    dark and theme issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hand_sign/screens/HomeScreen.dart';
 import 'package:flutter_hand_sign/screens/SplashScreen.dart';
+import 'package:flutter_hand_sign/services/UiProvider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
 
@@ -15,9 +17,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context)=>UiProvider()..init(),
+      child: Consumer<UiProvider>(
+        builder: (context, UiProvider notifier, child) {
+          return MaterialApp(
+
+            debugShowCheckedModeBanner: false,
+
+            title: 'Dark Theme',
+            //By default theme setting, you can also set system
+            // when your mobile theme is dark the app also become dark
+
+            themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+
+            //Our custom theme applied
+            darkTheme: notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
+
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+
+            home: SplashScreen(),
+
+          );
+        },
+      ),
     );
   }
 }
