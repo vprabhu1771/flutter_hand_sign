f5e3899a2d7dfd85718715e073e631ca1c94ab77
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-21 12:01:34 +0530

    AlphabetScreen UI update

--- lib/screens/AlphabetScreen.dart
+++ lib/screens/AlphabetScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/HandSign.dart';
+import 'AlphabetDetailScreen.dart';
 
 class AlphabetScreen extends StatefulWidget {
   const AlphabetScreen({super.key});
@@ -8,9 +10,20 @@ class AlphabetScreen extends StatefulWidget {
 }
 
 class _AlphabetScreenState extends State<AlphabetScreen> {
+  final List<HandSign> alphabetImages = List.generate(
+    26,
+        (index) => HandSign(
+      name: String.fromCharCode(65 + index), // Generates letters A-Z
+      image_path: 'assets/dataset/${String.fromCharCode(65 + index)}.JPG', // Reusing images for demo
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("Alphabet Hand Signs"),
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
@@ -20,9 +33,9 @@ class _AlphabetScreenState extends State<AlphabetScreen> {
             mainAxisSpacing: 10.0,
             childAspectRatio: 0.8,
           ),
-          itemCount: 26,
+          itemCount: alphabetImages.length,
           itemBuilder: (context, index) {
-            String letter = String.fromCharCode(index + 65); // A to Z
+            final handSign = alphabetImages[index];
             return Card(
               elevation: 3,
               shape: RoundedRectangleBorder(
@@ -31,15 +44,38 @@ class _AlphabetScreenState extends State<AlphabetScreen> {
               child: InkWell(
                 borderRadius: BorderRadius.circular(10),
                 onTap: () {
-                  // Add action for letter tap.
+                  // Add navigation or functionality on tap if needed
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AlphabetDetailScreen(
+                        title: 'Number ${handSign.name}',
+                        handSign: handSign,
+                      ),
+                    ),
+                  );
+                  //
+                  // ScaffoldMessenger.of(context).showSnackBar(
+                  //   SnackBar(content: Text('Selected: ${handSign.name}')),
+                  // );
                 },
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.abc, size: 40, color: Colors.blueAccent),
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          handSign.image_path,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ),
                     const SizedBox(height: 8),
                     Text(
-                      letter,
+                      handSign.name,
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
